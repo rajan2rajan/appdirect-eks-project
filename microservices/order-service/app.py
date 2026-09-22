703095f002bf944f90e554823a6b31f1a4fb17a2
Rajan Bhandari <rajanbhandari939@gmail.com>
2026-09-23 06:25:44 +1000

    Update app.py

--- microservices/order-service/app.py
+++ microservices/order-service/app.py
@@ -1,1 +1,25 @@
 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "service": "order-service",
+        "message": "Order Service is running"
+    })
+
+@app.route("/api/orders")
+def orders():
+    return jsonify([
+        {"id": 101, "product": "Laptop", "status": "Completed"},
+        {"id": 102, "product": "Phone", "status": "Processing"}
+    ])
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy", "service": "order-service"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
